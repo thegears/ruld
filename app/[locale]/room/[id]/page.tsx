0c09ad1d9c56fece395fc4712f4c709c20f14fb6
thegears <semh.1362@gmail.com>
2026-05-25 00:07:55 +0300

    feat: room waiting page

--- app/[locale]/room/[id]/page.tsx
+++ app/[locale]/room/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { getRoom } from "@/app/actions/room";
 import Waiting from "@/components/room/waiting";
+import { getLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
 
 export default async function RoomPage({
@@ -10,10 +11,12 @@ export default async function RoomPage({
   const { id } = await params;
 
   const room = await getRoom(id);
+  const locale = await getLocale();
 
   if (room.error) return notFound();
+  const url = `${process.env.NEXT_PUBLIC_APP_URL}/${locale}/room/${id}`;
 
-  if (!room.data.player_b_id) return <Waiting />;
+  if (!room.data.player_b_id) return <Waiting url={url} />;
 
   return <>selam</>;
 }
--- components/room/waiting.tsx
+++ components/room/waiting.tsx
@@ -1,3 +1,54 @@
-export default function Waiting() {
-  return <>wait</>;
+"use client";
+
+import { useTranslations } from "next-intl";
+
+import { Check, Copy } from "lucide-react";
+import { Button } from "../ui/button";
+import { useEffect, useState } from "react";
+
+export default function Waiting({ url }: { url: string }) {
+  const t = useTranslations("waiting");
+  const [copied, setCopied] = useState(false);
+  const [dots, setDots] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prev) => (prev === 3 ? 0 : prev + 1));
+    }, 500);
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <main className="min-h-screen   bg-[#0a0a0f] relative overflow-hidden  flex flex-col items-center justify-center gap-8 p-8">
+      <div className="flex justify-center flex-col gap-4 items-center">
+        <h1
+          className={`text-shadow-lg text-shadow-violet-900/70 text-muted-foreground text-2xl md:text-5xl font-black tracking-tighter`}
+        >
+          {t("waitingForFriend")} {".".repeat(dots)}
+        </h1>
+        <h3>{t("inviteYourFriend")}</h3>
+        <div className="flex relative">
+          <div
+            onClick={() => navigator.clipboard.writeText(url)}
+            className="border cursor-no-drop border-violet-900 flex items-center  text-center text-muted-foreground p-4 w-full "
+          >
+            <div className="flex-1 text-xs sm:text-lg ">{url}</div>
+          </div>
+          <Button
+            onClick={() => {
+              navigator.clipboard.writeText(url);
+              setCopied(true);
+              setTimeout(() => setCopied(false), 2000);
+            }}
+            variant="outline"
+            className="justify-self-end h-16"
+          >
+            {copied ? <Check /> : <Copy size={16} />}
+          </Button>
+        </div>
+
+        <p className="text-sm md:text-base text-muted-foreground max-w-sm text-center"></p>
+      </div>
+    </main>
+  );
 }
